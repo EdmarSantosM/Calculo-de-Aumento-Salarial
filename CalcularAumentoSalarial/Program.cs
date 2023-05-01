757595fd3f71d5268c1a606c2d628c28ae34e43c
EdmarSantosM <santosem4@hotmail.com>
2023-04-30 23:01:18 -0300

    Update Program.cs

--- CalcularAumentoSalarial/Program.cs
+++ CalcularAumentoSalarial/Program.cs
@@ -6,6 +6,16 @@ public class Program
         Console.WriteLine();
 
         Console.WriteLine($"\tCalule o valor de atualização sobre o salário");
+        Console.WriteLine(" ----------------------------------------------------------------- ");
+
+        Decimal salarioAtual = 0;
+        Console.Write($" Digite o valor atual do salário R$");
+        salarioAtual = Convert.ToDecimal(Console.ReadLine());
+
+        Decimal percentual = 0;
+        Console.Write($" Digite o valor percentual de cálculo base para acréscimo %");
+        percentual = Convert.ToDecimal(Console.ReadLine());
         
+
     }
 }
